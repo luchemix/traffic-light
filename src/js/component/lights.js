86d64a030241948d57277a5d92b031620c5cfa90
luchemix <luchemix@gmail.com>
2021-03-04 01:11:42 +0000

    function resolved

--- src/js/component/lights.js
+++ src/js/component/lights.js
@@ -1,86 +1,35 @@
 import React, { useState } from "react";
 
 export function Lights() {
-	let [redLight, turnOnRed] = useState(false);
-	let [yellowLight, turnOnYellow] = useState(false);
-	let [greenLight, turnOnGreen] = useState(false);
+	let [redLight, turnOnRed] = useState("red");
+	let [yellowLight, turnOnYellow] = useState("yellow");
+	let [greenLight, turnOnGreen] = useState("green");
 
-	if (redLight == false && yellowLight == false && greenLight == false) {
-		return (
-			<div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-danger"
-					style={({ height: "100px" }, { height: "100px" })}
-					onClick={() => turnOnRed(true)}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-warning"
-					style={({ height: "100px" }, { height: "100px" })}
-					onClick={() => turnOnYellow(true)}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-success"
-					style={({ height: "100px" }, { height: "100px" })}
-					onClick={() => turnOnGreen(true)}></div>
-			</div>
-		);
-	} else if (
-		redLight == true &&
-		yellowLight == false &&
-		greenLight == false
-	) {
-		return (
-			<div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary shadow-lg bg-danger"
-					style={
-						({ height: "100px" }, { height: "100px" })
-						// { box-shadow): "10px 10px 10px 10px 5px red" }
-					}
-					onClick={() => turnOnRed(false)}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-warning"
-					style={({ height: "100px" }, { height: "100px" })}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-success"
-					style={({ height: "100px" }, { height: "100px" })}></div>
-			</div>
-		);
-	} else if (
-		yellowLight == true &&
-		redLight == false &&
-		greenLight == false
-	) {
-		return (
-			<div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-danger"
-					style={({ height: "100px" }, { height: "100px" })}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-warning shadow-lg"
-					style={({ height: "100px" }, { height: "100px" })}
-					onClick={() => turnOnYellow(false)}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-success"
-					style={({ height: "100px" }, { height: "100px" })}></div>
-			</div>
-		);
-	} else if (
-		greenLight == true &&
-		redLight == false &&
-		yellowLight == false
-	) {
-		return (
-			<div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-danger"
-					style={({ height: "100px" }, { height: "100px" })}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-warning"
-					style={({ height: "100px" }, { height: "100px" })}></div>
-				<div
-					className="w-50 mx-auto mt-1 mb-2 rounded-circle border border-secondary bg-success shadow-lg"
-					style={({ height: "100px" }, { height: "100px" })}
-					onClick={() => turnOnGreen(false)}></div>
-			</div>
-		);
+	function cambio1() {
+		turnOnRed("red glow");
+		turnOnYellow("yellow");
+		turnOnGreen("green");
 	}
+
+	function cambio2() {
+		turnOnRed("red");
+		turnOnGreen("green");
+		turnOnYellow("yellow glow");
+	}
+
+	function cambio3() {
+		turnOnRed("red");
+		turnOnYellow("yellow");
+		turnOnGreen("green glow");
+	}
+
+	return (
+		<div className="lights">
+			<div className={redLight} onClick={cambio1}></div>
+
+			<div className={yellowLight} onClick={cambio2}></div>
+
+			<div className={greenLight} onClick={cambio3}></div>
+		</div>
+	);
 }
--- src/js/component/traffic.js
+++ src/js/component/traffic.js
@@ -4,18 +4,8 @@ import { Red } from "./red.js";
 
 export function TL() {
 	return (
-		<div className="container-fluid">
-			<div className="row">
-				<div className="col-5"></div>
-				<div
-					className="col-2 bg-light justify-content-center rounded border border-dark"
-					style={({ height: "400px" }, { width: "100px" })}>
-					{/* Aquí van los comps de las luces */}
-					<Lights />
-					{/* <Red /> */}
-				</div>
-				<div className="col-5"></div>
-			</div>
+		<div id="tlBody">
+			<Lights />
 		</div>
 	);
 }
